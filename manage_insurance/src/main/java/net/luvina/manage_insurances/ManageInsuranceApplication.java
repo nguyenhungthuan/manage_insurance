2c497b9742303b6b5a2637fc9ded2a2b1ea7a1ef
nguyenhungthuan <nguyenhungthuan@luvina.net>
2017-06-29 16:04:51 +0700

    doi ten thu muc

---
+++ manage_insurance/src/main/java/net/luvina/manage_insurances/ManageInsuranceApplication.java
@@ -0,0 +1,12 @@
+package net.luvina.manage_insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageInsuranceApplication.class, args);
+	}
+}
